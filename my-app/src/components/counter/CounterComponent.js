cc0f7844556152125ccae2dca4e65f56387643b3
ntienmanh <ntienmanh94@gmail.com>
2021-04-10 23:13:02 +0700

    update ui and responsive counter app

--- my-app/src/components/counter/CounterComponent.js
+++ my-app/src/components/counter/CounterComponent.js
@@ -6,11 +6,19 @@ import CounterRedux from "./counter-redux/CounterRedux";
 
 export default function CounterComponent() {
   return (
-    <div className="flex flex-col h-calc-bottom">
-      <CounterClassComponent />
-      <CounterFunctionalComponent />
-      <CounterImmerjs />
-      <CounterRedux />
+    <div className="bg-pink-400 h-full lg:h-screen">
+      <div className="section-title pt-16 mb-24">
+        <h2 className="text-normal tracking-widest text-3xl text-center mb-2">
+          My Counter
+        </h2>
+        <div className="w-24 h-1.5 bg-react m-auto"></div>
+      </div>
+      <div className="flex justify-around items-center flex-wrap">
+        <CounterClassComponent />
+        <CounterFunctionalComponent />
+        <CounterImmerjs />
+        <CounterRedux />
+      </div>
     </div>
   );
 }
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -2,14 +2,32 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      xsm: "320px",
+      xs: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {
       height: (theme) => ({
         "calc-bottom": "calc(100vh - 48px)", // Calc height for counter flex col with nav heigh 48px;
       }),
       flex: {
         "0020": "0 0 20%",
+        "0025": "0 0 25%",
         "0030": "0 0 30%",
+        "0040": "0 0 40%",
         "0050": "0 0 50%",
+        "0060": "0 0 60%",
+        "0080": "0 0 80%",
+      },
+      textColor: {
+        normal: "#102a42",
+        react: "#60dbfb",
+      },
+      colors: {
+        react: "#60dbfb",
       },
     },
   },
